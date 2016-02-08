a230495f3af2b27ae62b73aff67d2345933df1f2
Wayne Keenan <wayne.keenan@gmail.com>
2016-02-08 08:24:21 +0000

    added web bluetooth

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,19 @@
+// see: http://stackoverflow.com/questions/18821087/errors-when-running-ssl-with-grunt-server
+// see: http://www.akadia.com/services/ssh_test_certificate.html
 module.exports = function(grunt) {
 
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 		connect: {
+			options: {
+				hostname: 'localhost',
+				key: grunt.file.read('server.key').toString(),
+				cert: grunt.file.read('server.crt').toString(),
+				ca: grunt.file.read('server.crt').toString(),
+				passphrase: 'grunt',
+			},
 			example: {
+                protocol: 'https',
 				port: 3141,
 				base: '.'
 			}
