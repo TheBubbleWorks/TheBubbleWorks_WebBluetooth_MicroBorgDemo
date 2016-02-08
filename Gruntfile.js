a73145a512e577987eae1a534c65f3ef8a357b6c
Wayne Keenan <wayne.keenan@gmail.com>
2016-02-08 05:36:02 +0000

    dev webserver using grunt and grunt connect plugin

---
+++ Gruntfile.js
@@ -0,0 +1,17 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		connect: {
+			example: {
+				port: 3141,
+				base: '.'
+			}
+		}
+	});
+
+
+	grunt.loadNpmTasks('grunt-connect');
+	grunt.registerTask('default', 'connect:example');
+
+};
